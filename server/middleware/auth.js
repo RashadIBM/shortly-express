ada1f3abeec6c84518dc0ea26da7051a939ca6f7
NickRashad <nrsternes@gmail.com>
2019-03-30 00:48:24 -0500

    Pass first session test

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,64 +1,38 @@
 const models = require('../models');
 const Promise = require('bluebird');
 
-// module.exports.createSession = (req, res, next) => {
-var createSession = (req, res, next) => {
+module.exports.createSession = (req, res, next) => {
   /**
    *  1. Verify if any cookies are present
    *  2. If none present then => initialize new session
    *  3. Set new cookie => res.cookie()
    *  4. Else =>
    */
-  var loadHash = () => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    if (Object.keys(req.cookies).length) {
+      req.session = {hash: 'Hmmm, wonder how this will be refactored'};
+      resolve(req);
+      next();
+    } else {
       models.Sessions.create()
         .then((successPacket) => {
-          // resolve(successPacket);
           // Pass an options object to get the hash we just created
           var options = {
             id: successPacket.insertId
           };
           models.Sessions.get(options)
             .then((results) => {
-              console.log('\n\nLoadHashComplete');
-              resolve(results.hash);
-              // req.session = {hash: results.hash};
+              req.session = {hash: results.hash};
+              resolve(req);
+              next();
             });
         });
-    });
-  };
 
-  if (Object.keys(req.cookies).length) {
-    req.session = {hash: 'Hmmm, wonder how this will be refactored'};
-  } else {
-    var hash = loadHash();
-    req.session = {hash};
-  }
-  next();
+    }
+  });
 
-  // if (Object.keys(req.cookies).length) {
-  //   req.session = {hash: 'Hmmm, wonder how this will be refactored'};
-  // } else {
-  //   models.Sessions.create()
-  //     .then((successPacket) => {
-  //       resolve(successPacket);
-  //       // Pass an options object to get the hash we just created
-  //       var options = {
-  //         id: successPacket.insertId
-  //       };
-  //       models.Sessions.get(options)
-  //         .then((results) => {
-  //           console.log('\n\n\n\n\n\n\n--->', results);
-  //           req.session = {hash: results.hash};
-  //         });
-  //     });
-
-  // }
-  // next();
 };
 
-createSession = Promise.promisify(createSession);
-module.exports.createSession = createSession;
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
